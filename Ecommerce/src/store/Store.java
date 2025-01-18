1e473287f6099dc6bc5811c9f1d7ac25c70a0948
nurakmal09 <nurakmalhamdi@gmail.com>
2025-01-19 01:11:09 +0800

    Update Store.java

--- Ecommerce/src/store/Store.java
+++ Ecommerce/src/store/Store.java
@@ -126,7 +126,6 @@ public class Store {
 
     // function for creating customer 
     public static Customer createCustomer(){
-        typeOfUser = "Customer";
         Scanner sc = new Scanner(System.in);
         Customer current = new Customer();
         System.out.println("What is your userId?");
